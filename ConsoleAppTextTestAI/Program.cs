06960b93904f1f276d16c1c5444028724ed79aee
philnbr <phnaber@web.de>
2022-12-29 19:38:56 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleAppTextTestAI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleAppTextTestAIML.Model;
+
+namespace ConsoleAppTextTestAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userdata = Console.ReadLine();
+
+            // Add input data
+            var input = new ModelInput();
+            input.Email = userdata;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine(result.Prediction);
+        }
+    }
+}
